9dadbed646ac64b115c2b2463a0d752e886b995a
createwithshaikh-code <createwithshaikh@gmail.com>
2026-04-15 08:10:45 +0600

    auto: update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -260,7 +260,7 @@ const Hero = forwardRef<HeroHandle, { onEnterOrbit?: () => void }>(
         }
 
         #layer-horizon {
-          position:absolute;bottom:-16vh;left:0;width:100%;height:60vh;z-index:4;
+          position:absolute;bottom:-16vh;left:0;width:100%;height:60vh;z-index:7;
           backface-visibility:hidden;
         }
         @media (min-aspect-ratio:16/9){ #layer-horizon { bottom:-36vh; } }
